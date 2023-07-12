60a684aa36778ba92f364f702957089e6d694cd6
DanilMargaryan <margaryan.danil@mail.ru>
2023-07-12 18:33:13 +0300

    add model

--- cleanworkplace/club/models.py
+++ cleanworkplace/club/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Club(models.Model):
+    address = models.CharField(max_length=100)
+
+
+class Camera(models.Model):
+    ip = models.CharField(max_length=200)
+    location = models.CharField(max_length=150, null=True, blank=True)
+    club = models.ForeignKey(Club, on_delete=models.CASCADE)
+
+
+class Picture(models.Model):
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time = models.DateTimeField(auto_now_add=True)
+    camera = models.ForeignKey(Camera, on_delete=models.CASCADE)
+
--- cleanworkplace/club/views.py
+++ cleanworkplace/club/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+menu = [{'title': "КОЛИЗЕЙ", 'url_name': 'about'},
+        {'title': "login", 'url_name': 'addpage'},
+        ]
+
 
 def index(request):
     return render(request, 'club/index.html')
